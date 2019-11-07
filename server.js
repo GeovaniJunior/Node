8c78bfc9b80778dd863e4b20f458e629ae01f427
unknown <gdcarvalhojr@gmail.com>
2019-11-07 00:00:35 -0300

    Adicionando Express ao projeto

--- server.js
+++ server.js
@@ -1,15 +1,77 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer(function (req, resp){
-	resp.end(`
-		<html>
-			<head>
-				<meta charset="utf-8">
-			</head>
-			<body>
-				<h1> O Vitor quer dar o loló </h1>
-			</body>
-		</html>
-	`);
+app.listen(3000, function(){
+		console.log('Servidor rodando na porta 3000');
+})
+
+app.get('/', function(req, resp){
+	resp.send(
+	`
+ 				<html>
+ 					<head>
+ 						<meta charset="utf-8">
+ 					</head>
+ 					<body>
+ 						<h1> Casa do Código </h1>
+ 					</body>
+ 				</html>
+ 			`
+	);
+	
 });
-servidor.listen(3000);
+
+app.get('/livros', function(req, resp){
+	resp.send(
+	`
+ 				<html>
+ 					<head>
+ 						<meta charset="utf-8">
+ 					</head>
+ 					<body>
+ 						<h1> Listagem de Livros </h1>
+ 					</body>
+ 				</html>
+ 			`
+	);
+	
+});
+
+// const http = require('http');
+
+// const servidor = http.createServer(function (req, resp){
+	
+// 	let html = '';
+
+// 	if (req.url == '/'){
+		
+// 		html = `
+// 				<html>
+// 					<head>
+// 						<meta charset="utf-8">
+// 					</head>
+// 					<body>
+// 						<h1> Casa do Código </h1>
+// 					</body>
+// 				</html>
+// 			`;
+// 	} else if (req.url == '/livros') {
+// 	
+// 		
+// 		html = `
+// 				<html>
+// 					<head>
+// 						<meta charset="utf-8">
+// 					</head>
+// 					<body>
+// 						<h1> Listagem de Livros </h1>
+// 					</body>
+// 				</html>
+// 			`;
+// 	
+// 	}
+
+// 	resp.end(html);
+
+// });
+// servidor.listen(3000);
